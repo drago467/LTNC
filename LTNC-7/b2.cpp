71ffb2c4c9ea0b19dc89a5b1155cb95843bb4062
Trần Đình Tuấn <125422941+drago467@users.noreply.github.com>
2023-08-02 14:41:59 +0700

    backtrack

--- LTNC-7/b2.cpp
+++ LTNC-7/b2.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    char characters[] = {'a', 'b', 'c', 'd'};
-    int n = sizeof(characters) / sizeof(characters[0]);
-
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            for(int k = 0; k < n; k++) {
-                cout << characters[i] << characters[j] << characters[k] << endl;
+char arr[100];
+char characters[] = {'a', 'b', 'c', 'd'};
+void Try(int i) {
+    for (int j=0; j<4; j++) {
+        arr[i] = characters[j];
+        if (i==2) {
+            for (int k=0; k<3; k++) {
+                cout << arr[k];
             }
+            cout << endl;
+        }
+        else {
+            Try(i+1);
         }
     }
-
-    return 0;
+}
+int main() {
+    Try(0);
 }
